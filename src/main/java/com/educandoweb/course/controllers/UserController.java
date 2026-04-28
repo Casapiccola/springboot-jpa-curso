b7d7b864c9c5b617ebcfdd3e98059db893faf7eb
Erik Casapiccola <contato.erikcasapiccola@gmail.com>
2026-04-27 21:30:03 -0300

    controller name add

--- src/main/java/com/educandoweb/course/controllers/UserController.java
+++ src/main/java/com/educandoweb/course/controllers/UserController.java
@@ -1,4 +1,4 @@
-package com.educandoweb.course.resources;
+package com.educandoweb.course.controllers;
 
 import com.educandoweb.course.entities.User;
 import org.springframework.http.ResponseEntity;
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/users")
-public class UserResource {
+public class UserController {
 
     @GetMapping
     public ResponseEntity<User> findAll() {
